6754b838123a255e5a3add7780b9290c70651b00
Robert Garner <rgarner011235@gmail.com>
2024-11-12 19:01:24 -0700

    Added model classes

---
+++ DataDemo/Models/School.cs
@@ -0,0 +1,34 @@
+﻿namespace DataDemo.Models
+{
+    class School
+    {
+        private List<string> course;
+        private List<string> major;
+        private List<string> campus;
+        private List<string> student;
+
+        public List<string> Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        public List<string> Major
+        {
+            get { return major; }
+            set { major = value; }
+        }
+
+        public List<string> Campus
+        {
+            get { return campus; }
+            set { campus = value; }
+        }
+
+        public List<string> Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+    }
+}
